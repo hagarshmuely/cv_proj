823f9afa60991ade598c221bcb3737fcf8819f48
hagarshmuely <hagarshmuely@gmail.com>
2024-03-21 16:56:39 +0200

    update 21 march

--- bonus_model.py
+++ bonus_model.py
@@ -2,8 +2,7 @@
 import torch
 from models import SimpleNet
 import torch.nn as nn
-from torchvision.models import resnet50
-from torchvision.models import resnet18
+from torchvision.models import googlenet, resnet50, resnet18
 # from trainer import write_output
 import torch.optim as optim
 from trainer import LoggingParameters, Trainer
@@ -25,19 +24,12 @@ def bonus_train():
     validation_dataset = load_dataset('fakes_dataset', 'val')
 
     model = build_resnet_backbone()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     trainer = Trainer(model=model, optimizer=optimizer,
                       criterion=nn.CrossEntropyLoss(), batch_size=16,
                       train_dataset=train_dataset,
                       validation_dataset=validation_dataset,
                       test_dataset=validation_dataset)
-
-    # optimizer = optim.Adam(model.parameters(), lr=0.001)
-    # trainer = Trainer(model=model, optimizer=optimizer,
-    #                   criterion=nn.CrossEntropyLoss(), batch_size=32,
-    #                   train_dataset=train_dataset,
-    #                   validation_dataset=validation_dataset,
-    #                   test_dataset=validation_dataset)
 
     optimizer_params = optimizer.param_groups[0].copy()
     # remove the parameter values from the optimizer parameters for a cleaner
@@ -54,10 +46,10 @@ def bonus_train():
 
 def build_resnet_backbone():
     # initialize your model:
-    model = resnet50(pretrained=True)
-    print(f'resnet50 params before change: {get_nof_params(model)}')
-    model.fc = nn.Linear(model.fc.in_features, 2)
-    print(f'resnet50 params after change: {get_nof_params(model)}')
+    model = googlenet(pretrained=True)
+    print(f'googlenet params before change: {get_nof_params(model)}')
+    model.fc = nn.Linear(model.fc.in_features, 5)
+    print(f'googlenet params after change: {get_nof_params(model)}')
 
     return model
 
@@ -78,5 +70,6 @@ if __name__ == "__main__":
     bonus_train()
 
 # python train_main.py -d fakes_dataset -m bonus_model --lr 0.01 -b 16 -e 3 -o SGD
-# python plot_accuracy_and_loss.py -m bonus_model -j out/fakes_dataset_bonus_model_SGD.json -d synthetic_dataset
+# python plot_accuracy_and_loss.py -m bonus_model -j out/fakes_dataset_bonus_model_SGD.json -d fakes_dataset
+# python numerical_analysis.py -m bonus_model -cpp checkpoints/bonus_model.pt -d fakes_dataset
 
